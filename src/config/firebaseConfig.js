5c1ecbaef0869f07850e68316f1f4cab92b57a7d
Fernando01santana <cool.fernando23@gmail.com>
2022-12-03 19:29:54 -0300

    feat: create configuration firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import "firebase/storage"
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.API_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const database = firebase.firestore()
+export default database
