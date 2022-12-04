ed5f101cb5e07d35410e9a57ffd5017443935938
Fernando01santana <cool.fernando23@gmail.com>
2022-12-04 13:46:08 -0300

    feat: create function edit task

---
+++ src/pages/Details/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import {View, Text,TextInput,TouchableOpacity} from "react-native"
+import styles from "./style"
+ import database from "../../config/firebaseConfig"
+
+export default function DetailTask({navigation, route}){
+
+    const [descriptionEdit,setDescriptionEdit] = useState(route.params.description)
+    const idTask = route.params.id
+ 
+    function editTask(description, id){
+      database.collection("Tasks").doc(id).update({
+        description: description,
+      })
+      navigation.navigate("Task")
+    }
+
+    return(
+        <View style={styles.container}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+          style={styles.input}
+          placeholder="Ex: estudar javascript"
+          onChangeText={setDescriptionEdit}
+          value={descriptionEdit}
+          />
+          <TouchableOpacity 
+            style={styles.buttonNewTask}
+            onPress={()=>{
+              editTask(descriptionEdit, idTask)
+            }}
+          >
+            <Text style={styles.iconButton}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      )
+}
