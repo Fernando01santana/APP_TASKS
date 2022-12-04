b7be88192b6b304787743992cfe8a0d373723ac6
Fernando01santana <cool.fernando23@gmail.com>
2022-12-04 01:32:55 -0300

    feat: create navigation

--- App.js
+++ App.js
@@ -1,27 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigation } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import Task from './src/pages/Task';
+import NewTask from './src/pages/NewTask';
+import DetailTask from './src/pages/Details';
 
 
-const Stack = createStackNavigation()
-
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Task'>
+        <Stack.Screen 
+         name="Activity Manager App"
+         component={Task}
+         options={{
+           headerTintColor:"#f92e6a"
+         }}
+        />
 
+        <Stack.Screen 
+         name="New Task"
+         component={NewTask}
+         options={{
+           headerTintColor:"#f92e6a"
+         }}
+        />
+
+        <Stack.Screen 
+         name="Details"
+         component={DetailTask}
+         options={{
+           headerTintColor:"#f92e6a"
+         }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
